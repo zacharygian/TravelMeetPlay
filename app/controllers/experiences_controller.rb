1edee521ca4ab3c7f94c308ca5bc058fec1cb4cc
Nozomi Okuma <skyidentity@gmail.com>
2018-03-06 16:58:42 +0900

    added basic CRUD features to experience

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,32 +1,46 @@
 class ExperiencesController < ApplicationController
-  before_action :set_experience, only: [:show, :create, :edit, :update, :destroy]
+  before_action :set_experience, only: [:show, :edit, :update, :destroy]
 
   def index
-    @experiences = Experience.all
+    @experiences = policy_scope(Experience)
   end
 
   def show
+    authorize @experience
   end
 
   def new
+    @user = current_user
     @experience = Experience.new
+    authorize @experience
   end
 
   def create
     @experience = Experience.new(find_params)
     @experience.user = current_user
-    @experience.save
+    authorize @experience
+
+    if @experience.save
+      redirect_to experiences_path
+    else
+      render :new
+    end
   end
 
   def edit
+    authorize @experience
   end
 
   def update
     @experience.update(find_params)
+    authorize @experience
+    redirect_to experiences_path
   end
 
   def destroy
     @experience.destroy
+    authorize @experience
+    redirect_to experiences_path
   end
 
   private
@@ -36,7 +50,6 @@ class ExperiencesController < ApplicationController
   end
 
   def find_params
-    params.require(:experience).permit(:user_id, :sport_id, :skill_level)
+    params.require(:experience).permit(:sport_id, :skill_level)
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Experience.destroy_all
 user = User.first
 puts(user.id)
 
-Sport.new(name: "tennis")
+Sport.create(name: "tennis")
 
-  Experience.new(user_id: user, sport_id: 1, skill_level: 3).save
+  Experience.create!(user_id: user, sport_id: 1, skill_level: 3)
   puts "created a experience!"
