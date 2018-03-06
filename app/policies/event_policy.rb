442f99a92ce2b61f6de3a903e3df64c5ac2e18e7
Zachary Gian <z.gian12@eabjm.org>
2018-03-06 14:52:40 +0900

    worked on controller for events

---
+++ app/policies/event_policy.rb
@@ -0,0 +1,31 @@
+class EventPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def dashboard?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
