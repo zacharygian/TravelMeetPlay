644c756576832c351714d0dce6dc1899ba5504bb
Nozomi Okuma <skyidentity@gmail.com>
2018-03-11 19:26:58 +0900

    deleted migration added to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311083454) do
+ActiveRecord::Schema.define(version: 20180308064128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 20180311083454) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "pending"
-    t.integer "rating", default: 0
-    t.string "review_content"
     t.index ["event_id"], name: "index_bookings_on_event_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
