df5206b9e548eb7f32cbc79d7486019732fcf02c
Nozomi Okuma <skyidentity@gmail.com>
2018-03-12 18:38:26 +0900

    facebook login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312044229) do
+ActiveRecord::Schema.define(version: 20180312085954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,11 @@ ActiveRecord::Schema.define(version: 20180312044229) do
     t.integer "age"
     t.string "country"
     t.text "biography"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
