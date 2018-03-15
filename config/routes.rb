0581e029e0ddf46999fde5dd00349846f0a919f9
Nozomi Okuma <skyidentity@gmail.com>
2018-03-15 17:55:07 +0900

    password check

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'registrations' }
   root to: 'pages#home'
 
   resources :experiences
