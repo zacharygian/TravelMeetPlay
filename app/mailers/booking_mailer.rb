8569358147481da5fb2a569f20db9be794110527
Nozomi Okuma <skyidentity@gmail.com>
2018-03-15 09:29:09 +0900

    send mail when owner accepts requests from participants

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -10,4 +10,10 @@ class BookingMailer < ApplicationMailer
 
      mail(to: @booking.event.host.email, subject: "You've got a approval request")
   end
+
+   def approval_mail(booking)
+    @booking = booking
+
+     mail(to: @booking.user.email, subject: "You've got approved")
+  end
 end
